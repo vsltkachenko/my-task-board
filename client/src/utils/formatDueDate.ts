b8032a99d90172d8304065f1835e4995e0718664
Vasyl Tkachenko <vasyltkachenko07@gmail.com>
2024-04-04 23:33:13 +0300

    Fixed date formatting

--- client/src/utils/formatDueDate.ts
+++ client/src/utils/formatDueDate.ts
@@ -1,11 +1,22 @@
 function formatDueDate(inputDate: string | undefined): string {
     if (!inputDate) return ''
 
+    const dateParts = inputDate.split('/')
+    const year = dateParts[2]
+    const month = new Date(
+        +dateParts[2],
+        +dateParts[0] - 1,
+        +dateParts[1]
+    ).toLocaleString('en-US', { month: 'short' })
+    const day = dateParts[1]
+
+    return `${getDayOfWeek(inputDate)}, ${day} ${month}`
+}
+
+function getDayOfWeek(inputDate: string): string {
+    const daysOfWeek = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat']
     const date = new Date(inputDate)
-    return date.toLocaleDateString('en-US', {
-        weekday: 'short',
-        day: 'numeric',
-        month: 'short'
-    })
+    const dayOfWeekIndex = date.getDay()
+    return daysOfWeek[dayOfWeekIndex]
 }
 export default formatDueDate
